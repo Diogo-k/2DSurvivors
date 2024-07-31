8e1daaf0b5b5ce9e0553886ff9849d2d36c2038c
Diogo Paulo <jdiogok@gmail.com>
2024-07-31 19:10:09 +0100

    Fix Restart button

--- scenes/UI/end_screen.gd
+++ scenes/UI/end_screen.gd
@@ -28,7 +28,7 @@ func on_restart_button_pressed():
 	ScreenTransition.transition()
 	await ScreenTransition.transition_halfway
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main_new/main_new.tscn")
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 func on_quit_button_pressed():
 	ScreenTransition.transition()
